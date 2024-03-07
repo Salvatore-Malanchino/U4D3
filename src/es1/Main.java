365bc7e44e899f6616c622d8de5d18429649cb14
Salvatore-Malanchino <malanchinos@hotmail.it>
2024-03-07 16:16:47 +0100

    Esercizio 1

--- src/es1/Main.java
+++ src/es1/Main.java
@@ -1,4 +1,40 @@
 package es1;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner myScanner=new Scanner(System.in);
+        System.out.println("Rettangolo 1");
+        Rettangolo rettangolo1 = new Rettangolo(myScanner);
+        stampaRettangolo(rettangolo1);
+        System.out.println("Rettangolo 2");
+        Rettangolo rettangolo2 = new Rettangolo(myScanner);
+
+        myScanner.close();
+    }
+
+
+    public static void stampaRettangolo(Rettangolo rettangolo) {
+        System.out.println("Area rettangolo: " + rettangolo.area());
+        System.out.println("Perimetro rettangolo: " + rettangolo.perimetro());
+    }
+    public static void stampaRettangolo2(Rettangolo rettangolo1, Rettangolo rettangolo2) {
+
+        System.out.println("Rettangolo 1");
+        stampaRettangolo(rettangolo1);
+        System.out.println("Rettangolo 2");
+        stampaRettangolo(rettangolo2);
+
+
+        int sommaPerimetro = rettangolo1.perimetro() + rettangolo2.perimetro();
+
+        int sommaArea = rettangolo1.area() + rettangolo2.area();
+
+        System.out.println("somma perimetro" );
+        System.out.println(sommaPerimetro);
+        System.out.println("somma area");
+        System.out.println(sommaArea);
+    }
+
 }
--- src/es1/Rettangolo.java
+++ src/es1/Rettangolo.java
@@ -1,7 +1,30 @@
 package es1;
 
-public class Rettangolo {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Rettangolo {
+
+   private int altezza;
+   private int larghezza;
+
+
+   public Rettangolo (Scanner myScanner){
+
+       System.out.println("inserisci l'altezza");
+       altezza=myScanner.nextInt();
+       System.out.println("inserisci larghezza");
+       larghezza=myScanner.nextInt();
+
+
+
+   }
+
+    public Rettangolo(int alt, int larg) {
+       this.altezza=alt;
+       this.larghezza=larg;
     }
+
+    public int area(){return altezza * larghezza;}
+   public int perimetro(){return (altezza+larghezza)*2;}
+
 }
